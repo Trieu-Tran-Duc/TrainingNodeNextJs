5e7ac8fdde3210a18144e98bad79a790e1c37c87
TrieuTD <tranductrieu662@gmail.com>
2026-01-07 08:53:01 +0700

    fix port

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,8 @@
 const app = require("./app");
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on port ${process.env.PORT}`)
-);
+// app.listen(process.env.PORT, () =>
+//   console.log(`Server running on port ${process.env.PORT}`)
+// );
+app.listen(4000, () => {
+  console.log("API running on port 4000");
+});
